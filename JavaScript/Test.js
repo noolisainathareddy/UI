990b5c3cc406771669d096479495c4270207167f
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2023-11-24 09:10:09 -0600

    JavaScript prac

---
+++ JavaScript/Test.js
@@ -0,0 +1,12 @@
+var person =1;
+// console.log(person);
+console.log(typeof person);
+//Data types number,String, boolean, object
+var num = 1.3;
+var city="venpalli";
+var isTrue = false;
+
+console.log(typeof num);
+console.log(typeof city);
+console.log(typeof isTrue);
+
